b106476deec97a043eeafe5f66eb11fc6800b76c
Hans Oesterholt <hans@oesterholt.net>
2013-05-04 22:58:59 +0200

    install

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ INSTDIR=/usr/share/perlshare
 
 # Install pershare server
 mkdir -p $INSTDIR
-tar cf - PerlShareServer.pl unison_mask etc init.d PerlShareCommon | (cd $INSTDIR; tar xf -)
+tar cf - PerlShareServer.pl unison_umask etc init.d PerlShareCommon | (cd $INSTDIR; tar xf -)
 chown -R root.root $INSTDIR/
-chmod 755 $INSTDIR/unison_mask
+chmod 755 $INSTDIR/unison_umask
 chmod 755 $INSTDIR/PerlShareServer.pl
 ln -s -f $INSTDIR/htdocs /var/www/perlshare
 chmod 644 $INSTDIR/htdocs/*
@@ -22,21 +22,9 @@ chmod 644 /etc/perlshare/*
 (cd /etc/apach2/conf.d/; ln -s -f /etc/perlshare/perlshare_apache.conf .)
 /etc/init.d/apache2 restart
 
-
 # Install init.d daemon starter
 cp $INSTDIR/init.d/perlshare /etc/init.d
 chown root.root /etc/init.d/perlshare
 chmod 755 /etc/init.d/perlshare
-
-#for i in 2 3 4 5 
-#do
-#  ln -s -f /etc/init.d/perlshare /etc/rc$i.d/S70perlshare
-#done
-
-#for i in 1 6
-#do
-#  ln -s -f /etc/init.d/perlshare /etc/rc$i.d/K10perlshare
-#done
-
 update-rc.d perlshare defaults
 
