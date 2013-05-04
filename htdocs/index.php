9fc20a65de788a14c9d4805e6ca0e3888f72dd94
Hans Oesterholt <hans@oesterholt.net>
2013-05-04 22:49:12 +0200

    stuff

---
+++ htdocs/index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>PerlShare file sharing</title>
+</head>
+<body>
+<?php
+  echo "<p>HI!</p>";
+?>
+</body>
+</html>
--- install.sh
+++ install.sh
@@ -3,6 +3,15 @@
 # Install on debian systems
 INSTDIR = /usr/share/perlshare
 
+# Install pershare server
+mkdir $INSTDIR
+tar cf - PerlShareServer.pl unison_mask etc init.d PerlShareCommon | (cd /usr/share/perlshare; tar xf -)
+chown -R root.root $INSTDIR/
+chmod 755 $INSTDIR/unison_mask
+chmod 755 $INSTDIR/PerlShareServer.pl
+ln -s -f $INSTDIR/htdocs /var/www/perlshare
+chmod 644 $INSTDIR/htdocs/*
+
 # Install proxy configuration 
 a2enmod proxy
 a2enmod proxy_connect
@@ -10,7 +19,7 @@ mkdir -p /etc/perlshare
 cp $INSTDIR/etc/perlshare/* /etc/perlshare
 chown -R root.root /etc/perlshare
 chmod 644 /etc/perlshare/*
-(cd /etc/apach2/conf.d/; ln -s /etc/perlshare/perlshare_apache.conf .)
+(cd /etc/apach2/conf.d/; ln -s -f /etc/perlshare/perlshare_apache.conf .)
 /etc/init.d/apache2 restart
 
 
@@ -21,12 +30,12 @@ chmod 755 /etc/init.d/perlshare
 
 for i in 2 3 4 5 
 do
-  ln -s /etc/init.d/perlshare /etc/rc$i.d/S70perlshare
+  ln -s -f /etc/init.d/perlshare /etc/rc$i.d/S70perlshare
 done
 
 for i in 1 6
 do
-  ln -s /etc/init.d/perlshare /etc/rc$i.d/K10perlshare
+  ln -s -f /etc/init.d/perlshare /etc/rc$i.d/K10perlshare
 done
 
 update-rc.d perlshare defaults
