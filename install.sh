5ca0530c40f6995d6b70fa6754ebb2f5ab4abee8
Hans Oesterholt <hans@oesterholt.net>
2013-05-04 23:00:57 +0200

    install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ INSTDIR=/usr/share/perlshare
 
 # Install pershare server
 mkdir -p $INSTDIR
-tar cf - PerlShareServer.pl unison_umask etc init.d PerlShareCommon | (cd $INSTDIR; tar xf -)
+tar cf - PerlShareServer.pl unison_umask htdocs etc init.d PerlShareCommon | (cd $INSTDIR; tar xf -)
 chown -R root.root $INSTDIR/
 chmod 755 $INSTDIR/unison_umask
 chmod 755 $INSTDIR/PerlShareServer.pl
